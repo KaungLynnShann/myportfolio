b85bc787eaf266baf73a646aa178e556013c4fb6
Kaung Lynn Shann <153850401+KaungLynnShann@users.noreply.github.com>
2023-12-21 12:25:00 +0630

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,7 @@
       <h2>About Me</h2>
       <p>My Name is Kaung Lynn Shann.I'm learning <span style="color: tomato;font-size: 1.5em;">JAVA SPRING BOOT </span>
         and<span style="color:mediumseagreen;font-size: 1.5em;"> Vuejs</span>.
-        <br/>I wrote POS System project with team.Now,I'm still writting other project with myself.
+        <br/>I wrote POS System project with team.Now,I'm still writing other project with myself.
         <br/>I want to full stack developer.
       </p>
     </div>
