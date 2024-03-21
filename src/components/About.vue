48300809559d47cd13f88d4d82416739f33b3535
Kaung Lynn Shann <153850401+KaungLynnShann@users.noreply.github.com>
2024-03-21 17:17:17 +0630

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,12 +4,11 @@
      <p style="font-weight: bold;">
   <span style="color: blue;">Kaung Lynn Shann</span>, a passionate developer with a strong foundation in 
   <span style="color: tomato;">Java Spring Boot</span> and 
-  <span style="color: mediumseagreen;">Vue.js</span>.
+  <span style="color: mediumseagreen;">Vue.js.</span>
 </p><p>
-  I'm a highly motivated and results-oriented individual with a knack for problem-solving and a dedication to continuous learning. 
-  My strong analytical skills allow me to break down complex challenges and develop efficient solutions. 
-  I'm also a team player, always eager to collaborate and learn from others.
-  I'm actively seeking new challenges to further expand my skillset.
+  I'm a highly motivated and results-oriented individual with a knack for problem-solving and a dedication to continuous learning.
+  My strong analytical skills allow me to break down complex challenges and develop efficient solutions.
+  I'm also a team player, always eager to collaborate and learn from others. I'm actively seeking new challenges to further expand my skillset.
 </p>
     </div>
   </template>
